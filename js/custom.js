87c60331f4d4d2a20e8ee31641cd91ca40e10ff5
Andrei Nemeti <nemetimarian@yahoo.com>
2018-06-03 22:14:17 +0300

    fixed mobile menu bug
    The menu was closing when the user clicked anywhere on 'body' due to evt
    prop. Now the menu closes only when you press the 'close-btn-container'

--- js/custom.js
+++ js/custom.js
@@ -1,198 +1,140 @@
-var wow = new WOW(
-      {
-      animateClass: 'animated',
-      mobile: false,
-      offset: 50
+var wow = new WOW({
+    animateClass: 'animated',
+    mobile: false,
+    offset: 50
+});
+wow.init();
+
+$(document).ready(function() {
+    "use strict";
+
+
+
+
+    //wow.js on scroll animations initialization
+
+
+    var header = $('.header');
+    var hamb = $('.hamburger');
+    var hambActive = false;
+    var menuActive = false;
+
+    initHamburger();
+    initSlider();
+
+
+
+    function initHamburger() {
+      
+            hamb.on('click', function(event) {
+                event.stopPropagation();
+
+                if (!menuActive) {
+                    openMenu();
+                    $('body').css({
+                        overflow: 'hidden',
+
+                    });
+                    $('.close-btn-container').click(function() {
+                        closeMenu();
+                        $('body').css({
+                            'overflow-y': 'scroll',
+                        });
+                    });
+                } else {
+                    $('.mobile-menu').removeClass('active');
+                    menuActive = false;
+                }
+            });
+        }
+ 
+
+    function openMenu() {
+        var fs = $('.mobile-menu');
+        fs.addClass('active');
+        hambActive = true;
+        menuActive = true;
     }
-  );
-  wow.init();
 
-$(document).ready(function()
-{
-	"use strict";
+    function closeMenu() {
+        var fs = $('.mobile-menu');
+        fs.removeClass('active');
+        hambActive = false;
+        menuActive = false;
+    }
 
+    /* 
 
+    4. Set Header
 
+    */
 
-//wow.js on scroll animations initialization
-  
+    function initSlider() {
+        if ($('.special_slider').length) {
+            var specialSlider = $('.special_slider');
+            specialSlider.owlCarousel({
+                loop: true,
+                autoplay: false,
+                center: true,
+                stagePadding: 190,
+                margin: 5,
+                nav: false,
+                dots: false,
+                smartSpeed: 700,
+                responsive: {
+                    0: { items: 1, margin: 5, stagePadding: 0 },
+                    992: { items: 2, margin: 5, stagePadding: 130 },
+                    1280: { items: 3, margin: 5, stagePadding: 190 }
+                }
+            });
+        }
 
-	var header = $('.header');
-	var hamb = $('.hamburger');
-	var hambActive = false;
-	var menuActive = false;
+        if ($('.slider-nav-right').length) {
+            var next = $('.slider-nav-right');
+            next.on('click', function() {
+                specialSlider.trigger('next.owl.carousel');
+            });
+        }
 
-	initHamburger();
-	initSlider();
+        $('.slider-nav-left').click(function() {
 
-	initSearch();
-
-
-	function initHamburger()
-	{
-		if($('.hamburger').length)
-		{
-			hamb.on('click', function(event)
-			{
-				event.stopPropagation();
-
-				if(!menuActive)
-				{
-					openMenu();
-					$('body').css({
-						overflow: 'hidden',
-					
-					});
-					$(document).one('click', function cls(e)
-					{
-						if($(e.target).hasClass('menu_mm'))
-						{
-							$(document).one('click', cls);
-						}
-						else
-						{
-							closeMenu();
-							$('body').css({
-						'overflow-y': 'scroll',
-					
-					});
-						}
-					});
-				}
-				else
-				{
-					$('.mobile-menu').removeClass('active');
-					menuActive = false;
-				}
-			});
-		}
-	}
-
-	function openMenu()
-	{
-		var fs = $('.mobile-menu');
-		fs.addClass('active');
-		hambActive = true;
-		menuActive = true;
-	}
-
-	function closeMenu()
-	{
-		var fs = $('.mobile-menu');
-		fs.removeClass('active');
-		hambActive = false;
-		menuActive = false;
-	}
-
-	/* 
-
-	4. Set Header
-
-	*/
-
-	function initSlider()
-	{
-		if($('.special_slider').length)
-		{
-			var specialSlider = $('.special_slider');
-			specialSlider.owlCarousel(
-			{
-				loop:true,
-				autoplay:false,
-				center:true,
-				stagePadding:190,
-				margin:5,
-				nav:false,
-				dots:false,
-				smartSpeed:700,
-				responsive:
-				{
-					0:{items:1,margin:5,stagePadding:0},
-					992:{items:2,margin:5,stagePadding:130},
-					1280:{items:3,margin:5,stagePadding:190}
-				}
-			});
-		}
-
-		if($('.slider-nav-right').length)
-		{
-			var next = $('.slider-nav-right');
-			next.on('click', function()
-			{
-				specialSlider.trigger('next.owl.carousel');
-			});
-		}
-
-		$('.slider-nav-left').click(function() {
-
-				specialSlider.trigger('prev.owl.carousel');
-})
-	}
-
-	/* 
-
-	5. Init Video
-
-	*/
-
-	
-
-	/* 
-
-	6. Init Search
-
-	*/
-
-	function initSearch()
-	{
-		if($('.search').length)
-		{
-			var search = $('.search');
-			search.on('click', function(e)
-			{
-				var target = $(e.target);
-				if(!target.hasClass('ctrl_class'))
-				{
-					$(this).toggleClass('active');
-				}
-			});
-		}
-	}
+            specialSlider.trigger('prev.owl.carousel');
+        })
+    }
 
 
-	// 7.slow scrolling
 
-	//slow scrolling when clicking on links
+    // 7.slow scrolling
+
+    //slow scrolling when clicking on links
     $(document).ready(function() {
         $("a").on('click', function(event) {
             if (this.hash !== "") {
-            	if($(window).width()  > 767){
-                event.preventDefault();
-                var hash = this.hash;
+                if ($(window).width() > 767) {
+                    event.preventDefault();
+                    var hash = this.hash;
 
-                $('html, body').animate({
+                    $('html, body').animate({
 
-                    scrollTop: $(hash).offset().top -90
-                }, 800, function() {
-                    window.location.hash = hash;
-                });
-            }else{
+                        scrollTop: $(hash).offset().top - 90
+                    }, 800, function() {
+                        window.location.hash = hash;
+                    });
+                } else {
+                    event.preventDefault();
+                    var hash = this.hash;
 
+                    $('html, body').animate({
 
-            	 event.preventDefault();
-                var hash = this.hash;
-
-                $('html, body').animate({
-
-                    scrollTop: $(hash).offset().top -70
-                }, 800, function() {
-                    window.location.hash = hash;
-                });
+                        scrollTop: $(hash).offset().top - 70
+                    }, 800, function() {
+                        window.location.hash = hash;
+                    });
 
 
 
+                }
             }
-        }
         });
     });
 });
